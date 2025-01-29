e38f6e18c595c0848ce05bbe2a9444fe28e3befa
Hugo Janssen <janssenlive@gmail.com>
2025-01-29 15:55:14 -0600

    Added trusted CSRF origin to settings

--- todo/settings.py
+++ todo/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://to-do-janssen.onrender.com']
